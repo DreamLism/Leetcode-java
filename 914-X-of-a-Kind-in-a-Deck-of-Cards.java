dbd31d31690e841d17eb44ca4e717581abb42b5d
DreamLism <807130808@qq.com>
2019-05-24 22:21:53 +0800

    集合类的应用,map统计值出现次数方法的更新

--- 914-X-of-a-Kind-in-a-Deck-of-Cards.java
+++ 914-X-of-a-Kind-in-a-Deck-of-Cards.java
@@ -56,7 +56,7 @@ class Solution {
     public boolean hasGroupsSizeX(int[] deck) {
         Map<Integer, Integer> count = new HashMap<>();
         int res = 0;
-        for (int i : deck) count.put(i, count.getOrDefault(i, 0) + 1);
+        for (int i : deck) count.put(i, count.getOrDefault(i, 0) + 1);     //一行顶好几行，就是统计数字出现次数
         for (int i : count.values()) res = gcd(i, res);
         return res > 1;
     }
